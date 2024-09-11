7a9b41e998a1cb7da2a93f38bdb9c236b6e65b4e
HarishB <harishb@quinnox.com>
2024-09-11 10:51:05 +0530

    deployment change fixes

--- projects/test-app/src/app/app.module.ts
+++ projects/test-app/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
-import { QyrusAppModule } from 'qyrus-app';
+// import { QyrusAppModule } from 'qyrus-app';
 @NgModule({
   declarations: [
     AppComponent,
@@ -10,7 +10,7 @@ import { QyrusAppModule } from 'qyrus-app';
   ],
   imports: [
     BrowserModule,
-    QyrusAppModule
+    // QyrusAppModule
   ],
   providers: [],
   bootstrap: [AppComponent]
